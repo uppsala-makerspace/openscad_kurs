43c1d5d47a218576e17ba835fb0f002c98d57740
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-07-23 07:43:34 +0200

    Work in docs folder

--- scripts/create_markdown_books.sh
+++ scripts/create_markdown_books.sh
@@ -18,16 +18,19 @@ if [[ "$PWD" =~ scripts$ ]]; then
     exit 42
 fi
 
+# Work in the docs folder
+cd docs || exit 42
+
 # Use 'extractmedia' to get the images too
 # Tip from https://stackoverflow.com/questions/39956497/pandoc-convert-docx-to-markdown-with-embedded-images#comment88380070_39961440
-pandoc -o docs/generated_10_mars_bok_1.md --extract-media=media docs/10_mars_bok_1.docx
-pandoc -o docs/generated_18_mars_bok_2.md --extract-media=media docs/18_mars_bok_2.docx
-pandoc -o docs/generated_21_juni_bok_3.md --extract-media=media docs/21_juni_bok_3.docx
+pandoc -o generated_10_mars_bok_1.md --extract-media=media 10_mars_bok_1.docx
+pandoc -o generated_18_mars_bok_2.md --extract-media=media 18_mars_bok_2.docx
+pandoc -o generated_21_juni_bok_3.md --extract-media=media 21_juni_bok_3.docx
 
 # Pandoc creates some ugly annotation for picture size
-sed -i -e 's/{width=".*in"//g' -e 's/height=".*in"}//g' docs/generated_10_mars_bok_1.md 
-sed -i -e 's/{width=".*in"//g' -e 's/height=".*in"}//g' docs/generated_18_mars_bok_2.md 
-sed -i -e 's/{width=".*in"//g' -e 's/height=".*in"}//g' docs/generated_21_juni_bok_3.md 
+sed -i -e 's/{width=".*in"//g' -e 's/height=".*in"}//g' generated_10_mars_bok_1.md 
+sed -i -e 's/{width=".*in"//g' -e 's/height=".*in"}//g' generated_18_mars_bok_2.md 
+sed -i -e 's/{width=".*in"//g' -e 's/height=".*in"}//g' generated_21_juni_bok_3.md 
 
 
 
